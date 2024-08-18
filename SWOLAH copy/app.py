4d6ebc2c772bbc81c842de89dcba153b362b24ec
vnydl0015 <123066424+vnydl0015@users.noreply.github.com>
2024-08-18 17:26:21 +1000

    Add files via upload

---
+++ SWOLAH copy/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/preferences')
+def preferences():
+    return render_template('preferences.html')
+
+@app.route('/buy')
+def buy():
+    return render_template('buy.html')
+
+@app.route('/account')
+def account_page():
+    return render_template('account.html')
+
+
+@app.route('/eligibility')
+def eligibility():
+    return render_template('eligibility.html')
+
+
+@app.route('/dropoff')
+def dropoff():
+    return render_template('dropoff.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
